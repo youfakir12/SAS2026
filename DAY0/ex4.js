505c8e48646d0d42290608de2fabd5ed03265188
youfakir <oufakiryassine2@gmail.com>
2026-09-23 10:49:52 +0100

    finiched ex4

---
+++ DAY0/ex4.js
@@ -0,0 +1,11 @@
+let score = 85;
+
+if (score >= 90) {
+    console.log("A");
+} else if (score >= 80) {
+    console.log("B");
+} else if (score >= 70) {
+    console.log("C");
+} else {
+    console.log("F");
+}
