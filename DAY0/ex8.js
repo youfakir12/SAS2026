e7edc0dd46499861fb178ed1beab49a167721a27
youfakir <oufakiryassine2@gmail.com>
2026-09-23 10:56:18 +0100

    finiched ex8

---
+++ DAY0/ex8.js
@@ -0,0 +1,8 @@
+let a = 30;
+let b = 0;
+let c = 60;
+if(a + b + c == 180 && a != 0 && b != 0 && c != 0){  
+    console.log("this is a triangle")
+}else{
+    console.log("this is not a triangle");
+}
